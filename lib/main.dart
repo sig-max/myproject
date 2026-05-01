523902fbee8279ba87affe71aa5404a72569819f
swatantra <swatantrasahu6021@gmail.com>
2026-05-01 16:17:47 +0530

    Use production flavor for release builds

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'app.dart';
 import 'config/flavor_config.dart';
 
 Future<void> main() async {
-  await runMedicalApp(AppFlavor.dev);
-} 
+  // Use production flavor for release builds, dev for others.
+  final flavor = kReleaseMode ? AppFlavor.prod : AppFlavor.dev;
+  await runMedicalApp(flavor);
+}
 
